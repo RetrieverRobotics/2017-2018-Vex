d16f68c17ec716cfce25e3beff6c6f388c9fed0c
Zabikn <zabikn1@umbc.edu>
2018-03-03 10:39:24 -0500

    V7 skills code 3/3/18

---
+++ V7/auton.c
@@ -0,0 +1,497 @@
+////////////////////////////////////////////////////////
+//
+// 45 second competition auton
+//
+////////////////////////////////////////////////////////
+
+// Blue preloads auton
+if (autonSelection == BLUE_PRIMARY) {
+  writeDebugStreamLine("Blue Preloads running");
+
+  motor[rollers] = ROLLERS_HOLD;
+  setLiftHeight(LIFT_SCHMEDIUM);
+  resetDrive();
+  driveIncremental(0);
+  resetGyro();
+  pointTurn(SensorValue[gyro]);
+  startTask(liftPIDTask);
+	startTask(drivePIDTask);
+
+  motor[mogo] = -127;
+  wait1Msec(400); // 400
+  driveIncremental(48);
+  extendMogo();
+
+  waitForPID(drivePID);
+  // tardDrive(60);
+  // wait1Msec(400);
+  // tardDrive(0);
+
+  // writeDebugStreamLine("her77");
+  intakeMogo();
+  setLiftHeight(LIFT_FLOOR_HEIGHT);
+  wait1Msec(300);
+  motor[rollers] = ROLLERS_OUT;
+///////////cone2
+  driveIncremental(8);
+  swingOut();
+  motor[swing] = -50;
+  motor[rollers] = ROLLERS_IN;
+
+  setLiftHeight(LIFT_FLOOR_HEIGHT);
+  waitForPID(liftPID);
+  wait1Msec(500);
+  motor[rollers] = ROLLERS_HOLD;
+
+  setLiftHeight(LIFT_CONE_2);
+  waitForPID(liftPID);
+
+  swingIn();
+  tardLift(-70);
+  wait1Msec(300);
+  motor[rollers] = ROLLERS_OUT;
+  tardLift(127);
+  wait1Msec(100);
+  setLiftHeight(LIFT_CONE_2);
+
+/////////////////////////////////////cone 3
+  driveIncremental(8);
+  swingOut();
+  motor[swing] = -50;
+  motor[rollers] = ROLLERS_IN;
+  // wait1Msec(500);
+
+  setLiftHeight(LIFT_FLOOR_HEIGHT);
+  waitForPID(liftPID);
+  wait1Msec(500);
+  motor[rollers] = ROLLERS_HOLD;
+
+  setLiftHeight(LIFT_CONE_3);
+  waitForPID(liftPID);
+
+  swingIn();
+  tardLift(-70);
+  wait1Msec(300);
+  motor[rollers] = ROLLERS_OUT;
+  tardLift(127);
+  wait1Msec(100);
+  tardLift(0);
+  motor[rollers] = -ROLLERS_HOLD;
+
+  ///////////////////////////////////dropoff
+  driveIncremental(-60);
+  waitForPID(drivePID);
+  pointTurn(45);
+  waitForPID(gyroPID);
+  driveIncremental(-26);
+  waitForPID(drivePID);
+
+  pointTurn(135);
+  waitForPID(gyroPID);
+
+  // setLiftHeight(LIFT_SCHMEDIUM);
+  // motor[rollers] = ROLLERS_OUT;
+
+  tardDrive(127);
+  wait1Msec(400);
+  tardDrive(60);
+  wait1Msec(500);
+  tardDrive(0);
+  extendMogo();
+
+  ///back up
+  driveIncremental(-18);
+  waitForPID(drivePID);
+
+  pointTurn(135);
+  waitForPID(gyroPID);
+
+  tardDrive(25);
+  intakeMogo();
+  wait1Msec(200);
+  tardDrive(0);
+  // setGyro(-135);
+
+  ////////////////////////////mogo 2
+  driveIncremental(-9);
+  waitForPID(drivePID);
+  // motor[mogo] = -127;
+  // extendMogo();
+  pointTurn(45);
+  waitForPID(gyroPID);
+
+  driveIncremental(-35);
+  extendMogo();
+  setLiftHeight(LIFT_FLOOR_HEIGHT);
+  waitForPID(liftPID);
+  waitForPID(drivePID, 4000);
+  swingTurnRight(0);
+  waitForPID(gyroPID, 3000);
+
+  tardDrive(-90);
+  wait1Msec(600);
+  tardDrive(0);
+
+  wait1Msec(600);
+
+  setGyro(0);
+
+  driveIncremental(1);
+  setLiftHeight(LIFT_CONE_2);
+  waitForPID(drivePID);
+  pointTurn(-90);
+  extendMogo();
+  waitForPID(gyroPID);
+
+  // extendMogo();
+  driveIncremental(37);//36
+  waitForPID(drivePID);
+  intakeMogo();
+
+  ///////////cone4
+  driveIncremental(6);
+  swingOut();
+  motor[swing] = -50;
+  motor[rollers] = ROLLERS_IN;
+
+  setLiftHeight(LIFT_FLOOR_HEIGHT);
+  waitForPID(liftPID);
+  wait1Msec(500);
+  motor[rollers] = ROLLERS_HOLD;
+
+  setLiftHeight(LIFT_CONE_2);
+  waitForPID(liftPID);
+
+  swingIn();
+  tardLift(-70);
+  wait1Msec(300);
+  motor[rollers] = ROLLERS_OUT;
+  tardLift(127);
+  wait1Msec(100);
+  setLiftHeight(LIFT_CONE_2);
+
+/////////////////////////////////////cone 5
+  driveIncremental(6);
+  swingOut();
+  motor[swing] = -50;
+  motor[rollers] = ROLLERS_IN;
+  // wait1Msec(500);
+
+  setLiftHeight(LIFT_FLOOR_HEIGHT);
+  waitForPID(liftPID);
+  wait1Msec(500);
+  motor[rollers] = ROLLERS_HOLD;
+
+  setLiftHeight(LIFT_CONE_2);
+  waitForPID(liftPID);
+
+  swingIn();
+  tardLift(-70);
+  wait1Msec(300);
+  motor[rollers] = ROLLERS_OUT;
+  tardLift(127);
+  wait1Msec(100);
+  tardLift(0);
+  motor[rollers] = -ROLLERS_HOLD;
+
+  ///////////////////////////////////dropoff
+  driveIncremental(-60);
+  waitForPID(drivePID);
+  swingTurnRight(-225);
+  waitForPID(gyroPID);
+
+  // setLiftHeight(LIFT_SCHMEDIUM);
+  // motor[rollers] = ROLLERS_OUT;
+  tardDrive(26);
+  extendMogo();
+  tardDrive(0);
+
+  //////////////////////////////mogo3
+  driveIncremental(-12);
+  waitForPID(drivePID);
+  swingTurnRight(-315);
+  intakeMogo();
+  waitForPID(gyroPID);
+  driveIncremental(-5);//12?
+  waitForPID(drivePID);
+  swingTurnRight(-396);
+  waitForPID(gyroPID);
+  // extendMogo();
+
+  driveIncremental(60);
+  waitForPID(drivePID);
+  driveIncremental(-24);
+  extendMogo();
+  waitForPID(drivePID);
+
+  pointTurn(-370);
+  waitForPID(gyroPID);
+  driveIncremental(24);
+  waitForPID(drivePID);
+  intakeMogo();
+
+  // driveIncremental(-36);//-28 prev
+  // waitForPID(drivePID, 4000);
+  // pointTurn(-270);
+  // waitForPID(gyroPID);
+  //
+  // driveIncremental(16);
+  // waitForPID(drivePID, 3000);
+  //
+  // tardDrive(26);
+  // extendMogo();
+  // wait1Msec(300);
+  // tardDrive(0);
+  // driveIncremental(-16);
+
+
+  while(1){wait1Msec(1000);}
+
+}
+// Red preloads auton
+else if (autonSelection == RED_PRIMARY) {
+  writeDebugStreamLine("Red Preloads running");
+
+  motor[rollers] = ROLLERS_HOLD;
+  setLiftHeight(LIFT_SCHMEDIUM);
+  resetDrive();
+  driveIncremental(0);
+  resetGyro();
+  pointTurn(SensorValue[gyro]);
+  startTask(liftPIDTask);
+	startTask(drivePIDTask);
+
+  motor[mogo] = -127;
+  wait1Msec(400); // 400
+  driveIncremental(48);
+  extendMogo();
+
+  waitForPID(drivePID);
+  // tardDrive(60);
+  // wait1Msec(400);
+  // tardDrive(0);
+
+  // writeDebugStreamLine("her77");
+  intakeMogo();
+  setLiftHeight(LIFT_FLOOR_HEIGHT);
+  wait1Msec(300);
+  motor[rollers] = ROLLERS_OUT;
+///////////cone2
+  driveIncremental(8);
+  swingOut();
+  motor[swing] = -50;
+  motor[rollers] = ROLLERS_IN;
+
+  setLiftHeight(LIFT_FLOOR_HEIGHT);
+  waitForPID(liftPID);
+  wait1Msec(500);
+  motor[rollers] = ROLLERS_HOLD;
+
+  setLiftHeight(LIFT_CONE_2);
+  waitForPID(liftPID);
+
+  swingIn();
+  tardLift(-70);
+  wait1Msec(300);
+  motor[rollers] = ROLLERS_OUT;
+  tardLift(127);
+  wait1Msec(100);
+  setLiftHeight(LIFT_CONE_2);
+
+/////////////////////////////////////cone 3
+  driveIncremental(8);
+  swingOut();
+  motor[swing] = -50;
+  motor[rollers] = ROLLERS_IN;
+  // wait1Msec(500);
+
+  setLiftHeight(LIFT_FLOOR_HEIGHT);
+  waitForPID(liftPID);
+  wait1Msec(500);
+  motor[rollers] = ROLLERS_HOLD;
+
+  setLiftHeight(LIFT_CONE_3);
+  waitForPID(liftPID);
+
+  swingIn();
+  tardLift(-70);
+  wait1Msec(300);
+  motor[rollers] = ROLLERS_OUT;
+  tardLift(127);
+  wait1Msec(100);
+  tardLift(0);
+  motor[rollers] = -ROLLERS_HOLD;
+
+  ///////////////////////////////////dropoff
+  driveIncremental(-60);
+  waitForPID(drivePID);
+  pointTurn(-45);
+  waitForPID(gyroPID);
+  driveIncremental(-26);
+  waitForPID(drivePID);
+
+  pointTurn(-135);
+  waitForPID(gyroPID);
+
+  // setLiftHeight(LIFT_SCHMEDIUM);
+  // motor[rollers] = ROLLERS_OUT;
+
+  tardDrive(127);
+  wait1Msec(400);
+  tardDrive(60);
+  wait1Msec(500);
+  tardDrive(0);
+  extendMogo();
+
+  ///back up
+  driveIncremental(-18);
+  waitForPID(drivePID);
+
+  pointTurn(-135);
+  waitForPID(gyroPID);
+
+  tardDrive(25);
+  intakeMogo();
+  wait1Msec(200);
+  tardDrive(0);
+  // setGyro(-135);
+
+  ////////////////////////////mogo 2
+  driveIncremental(-9);
+  waitForPID(drivePID);
+  // motor[mogo] = -127;
+  // extendMogo();
+  pointTurn(-45);
+  waitForPID(gyroPID);
+
+  driveIncremental(-35);
+  extendMogo();
+  setLiftHeight(LIFT_FLOOR_HEIGHT);
+  waitForPID(liftPID);
+  waitForPID(drivePID, 4000);
+  swingTurnLeft(0);
+  waitForPID(gyroPID, 3000);
+
+  tardDrive(-90);
+  wait1Msec(600);
+  tardDrive(0);
+
+  wait1Msec(300);
+
+  setGyro(0);
+
+  driveIncremental(1);
+  setLiftHeight(LIFT_CONE_2);
+  waitForPID(drivePID);
+  pointTurn(90);
+  extendMogo();
+  waitForPID(gyroPID);
+
+  // extendMogo();
+  driveIncremental(37);//36
+  waitForPID(drivePID);
+  intakeMogo();
+
+  ///////////cone4
+  driveIncremental(6);
+  swingOut();
+  motor[swing] = -50;
+  motor[rollers] = ROLLERS_IN;
+
+  setLiftHeight(LIFT_FLOOR_HEIGHT);
+  waitForPID(liftPID);
+  wait1Msec(500);
+  motor[rollers] = ROLLERS_HOLD;
+
+  setLiftHeight(LIFT_CONE_2);
+  waitForPID(liftPID);
+
+  swingIn();
+  tardLift(-70);
+  wait1Msec(300);
+  motor[rollers] = ROLLERS_OUT;
+  tardLift(127);
+  wait1Msec(100);
+  setLiftHeight(LIFT_CONE_2);
+
+/////////////////////////////////////cone 5
+  driveIncremental(6);
+  swingOut();
+  motor[swing] = -50;
+  motor[rollers] = ROLLERS_IN;
+  // wait1Msec(500);
+
+  setLiftHeight(LIFT_FLOOR_HEIGHT);
+  waitForPID(liftPID);
+  wait1Msec(500);
+  motor[rollers] = ROLLERS_HOLD;
+
+  setLiftHeight(LIFT_CONE_2);
+  waitForPID(liftPID);
+
+  swingIn();
+  tardLift(-70);
+  wait1Msec(300);
+  motor[rollers] = ROLLERS_OUT;
+  tardLift(127);
+  wait1Msec(100);
+  tardLift(0);
+  motor[rollers] = -ROLLERS_HOLD;
+
+  ///////////////////////////////////dropoff
+  driveIncremental(-60);
+  waitForPID(drivePID);
+  swingTurnLeft(225);
+  waitForPID(gyroPID);
+
+  // setLiftHeight(LIFT_SCHMEDIUM);
+  // motor[rollers] = ROLLERS_OUT;
+  tardDrive(26);
+  extendMogo();
+  tardDrive(0);
+
+  //////////////////////////////mogo3
+  driveIncremental(-12);
+  waitForPID(drivePID);
+  swingTurnLeft(315);
+  intakeMogo();
+  waitForPID(gyroPID);
+  driveIncremental(-6);//12?
+  waitForPID(drivePID);
+  swingTurnLeft(396);
+  waitForPID(gyroPID);
+  // extendMogo();
+
+  driveIncremental(60);
+  waitForPID(drivePID);
+  driveIncremental(-24);
+  extendMogo();
+  waitForPID(drivePID);
+
+  pointTurn(370);
+  waitForPID(gyroPID);
+  driveIncremental(24);
+  waitForPID(drivePID);
+  intakeMogo();
+
+  // driveIncremental(-36);//-28 prev
+  // waitForPID(drivePID, 4000);
+  // pointTurn(-270);
+  // waitForPID(gyroPID);
+  //
+  // driveIncremental(16);
+  // waitForPID(drivePID, 3000);
+  //
+  // tardDrive(26);
+  // extendMogo();
+  // wait1Msec(300);
+  // tardDrive(0);
+  // driveIncremental(-16);
+
+
+  while(1){wait1Msec(1000);}
+
+}
+else if (1){
+
+}
---
+++ V7/usercontrol-2Controller.c
@@ -0,0 +1,278 @@
+////////////////////////////////////////////////////////
+//
+// Usercontrol utilizing the slave controller
+//
+////////////////////////////////////////////////////////
+
+clearTimer(T1);
+clearTimer(T2);
+SensorValue[driveLEnc] = 0;
+SensorValue[driveREnc] = 0;
+string displayString;
+bool   bSwingManual = false;
+bool   bSwingToggle = true;
+bool   bLiftJustUsed = false;
+bool   bLiftHeightRecorded = false;
+bool   bFlagRecordLift = false;
+int    liftSetPow = 0;
+int    currLiftHeight = getLiftHeight();
+int    lastLiftHeight = currLiftHeight;
+float  deltaHeight = 0;
+float  lastDeltaHeight = 0;
+bool rollerIdleIn = true;
+int swingTime = nSysTime;
+bool b7U2Toggle = true;
+bool b7D2Toggle = true;
+bool b7R2Toggle = true;
+bool liftMacroSet = false;
+bool swingInToggle = false;
+bool swingOutToggle = false;
+bool bRollerMacro = false;
+
+swingPID.target = SensorValue[swingPot];
+tardLift(0);
+// everything for the drive in usrctrl handled in this task.
+startTask(driveSlew);
+// startTask(swingPIDTask);
+startTask(liftPIDTask);
+
+bLCDBacklight = false;
+
+while (true) {
+  // Display battery level
+  clearLCDLine(0);
+  clearLCDLine(1);
+  sprintf(displayString, "Main: %2.2fV", nAvgBatteryLevel/1000.);
+  displayLCDCenteredString(0, displayString);
+  sprintf(displayString, "Backup: %2.2fV", BackupBatteryLevel/1000.);
+  displayLCDCenteredString(1, displayString);
+
+  currLiftHeight = getLiftHeight();
+  deltaHeight = currLiftHeight - lastLiftHeight;
+
+  //--------------------------------------------------------------------------------
+  // Macro buttons
+  //--------------------------------------------------------------------------------
+  if (!bSwingManual && vexRT[Btn7RXmtr2]) {
+    if(b7R2Toggle){
+      startTask(stackCone);
+      b7R2Toggle = false;
+    }
+    swingInToggle = false;
+    swingOutToggle = false;
+
+    liftMacroSet = true;
+    bRollerMacro = true;
+  }
+  else{
+    b7R2Toggle = true;
+  }
+
+  // floor height
+  // release and go up then back to floor height after tard down
+  // loader height
+
+  //--------------------------------------------------------------------------------
+  // Lift
+  //--------------------------------------------------------------------------------
+
+  if (vexRT[Btn8LXmtr2]){
+    setLiftHeight(LIFT_SCHMEDIUM);
+    liftMacroSet = true;
+  }
+  else if(vexRT[Btn8DXmtr2]) {
+    setLiftHeight(LIFT_SCHMEDIUM);
+    liftMacroSet = true;
+  }
+  // unset macros on these btns
+  else if (abs(vexRT[Ch2Xmtr2]) > LIFT_DEADBAND) {
+    if (liftMacroSet) {
+      tardLift(0);
+      liftMacroSet = false;
+    }
+  }
+
+  if(!liftMacroSet) {
+    // right joystick y axis on slave controller controls lift
+    liftSetPow = vexRT[Ch2Xmtr2];
+    // hold position inside deabands
+    if (fabs(liftSetPow) < LIFT_DEADBAND) {
+      // clear timer once immediately after button release
+      if (bLiftJustUsed) {
+        clearTimer(T2);
+        bLiftHeightRecorded = false; // set up to turn on pid later
+        tardLift(0);
+      }
+
+      // check conditions to set lift height
+      bFlagRecordLift = false;
+      // enable conditions
+      // look for local extrema of the height function and record position there
+      // if (sgn(deltaHeight) != sgn(lastDeltaHeight)) bFlagRecordLift = true;
+      // if (deltaHeight == 0) bFlagRecordLift = true;
+      if (vexRT[Btn8RXmtr2]) bFlagRecordLift = true;
+      // timeout after 500 ms
+      // if (time1[T2] > 500) bFlagRecordLift = true;
+
+      // disable conditions
+      // only turn on PIDs if the lift is up
+      if (currLiftHeight < LIFT_BLOCK_MOGO) bFlagRecordLift = false;
+
+      if (!bLiftHeightRecorded && bFlagRecordLift) {
+        setLiftHeight(currLiftHeight);
+        bLiftHeightRecorded = true;
+      }
+
+      bLiftJustUsed = false;
+    }
+    else {
+      tardLift(liftSetPow);
+      bLiftJustUsed = true;
+    }
+  }//END liftmacroset
+
+  //--------------------------------------------------------------------------------
+  // Rollers
+  //--------------------------------------------------------------------------------
+  if (vexRT[Btn6UXmtr2] || vexRT[Btn6DXmtr2]){
+    bRollerMacro = false;
+  }
+
+  // in on 6U
+  if (!bRollerMacro) {
+    if (vexRT[Btn6UXmtr2]) {
+      motor[rollers] = ROLLERS_IN;
+      rollerIdleIn = true;
+    }
+    // out on 6D
+    else if (vexRT[Btn6DXmtr2]) {
+      motor[rollers] = ROLLERS_OUT;
+      rollerIdleIn = false;
+    }
+    else {
+      if(rollerIdleIn) {
+        motor[rollers] = ROLLERS_HOLD;
+      }
+      else {
+        motor[rollers] = -ROLLERS_HOLD;
+      }
+    }
+  }
+  //--------------------------------------------------------------------------------
+  // Swing
+  //--------------------------------------------------------------------------------
+
+  //switch between manual and PID for swing on 7L
+  if (vexRT[Btn7LXmtr2]) {
+    if (bSwingToggle) {
+      bSwingManual = !bSwingManual;
+      bSwingToggle = false;
+    }
+  }
+  else {
+    bSwingToggle = true;
+  }
+
+  // if  manual control is selected
+  if (bSwingManual) {
+    swingPID.enabled = false;
+    // swing in on 7U
+    if (vexRT[Btn7DXmtr2]) {
+      motor[swing] = 127;
+    }
+    // swing out on 7D
+    else if (vexRT[Btn7UXmtr2]) {
+      motor[swing] = -127;
+    }
+    else {
+      // swing on Ch3Xmtr2
+      motor[swing] = -vexRT[Ch3Xmtr2];
+    }
+  }
+
+  //////////////////////////////
+  // otherwise use ?!not?PID? control
+  //////////////////////////////
+  else {
+    ////////////////////////////toggle buttons
+    // in on 7D
+    if (vexRT[Btn7DXmtr2]) {
+      if(b7D2Toggle){
+        swingTime = nSysTime;
+        b7D2Toggle = false;
+      }
+      swingInToggle = true;
+      swingOutToggle = false;
+    }
+    else {
+      b7D2Toggle = true;
+    }
+    // out on 7U
+    if (vexRT[Btn7UXmtr2]) {
+      if(b7U2Toggle){
+        swingTime = nSysTime;
+        b7U2Toggle = false;
+      }
+      swingInToggle = false;
+      swingOutToggle = true;
+    }
+    else {
+      b7U2Toggle = true;
+    }
+
+/////////////////////////macros
+    if (swingInToggle) {
+      if(nSysTime - swingTime < SWING_IN_TIME) {
+        motor[swing] = 127;
+      }
+      else {
+        motor[swing] = SWING_HOLD_POW;
+      }
+    }
+    else if (swingOutToggle) {
+      if(nSysTime - swingTime < SWING_OUT_TIME) {
+        motor[swing] = -127;
+      }
+      else {
+        motor[swing] = -SWING_HOLD_POW;
+      }
+    }
+
+  }//END not manual control
+
+  //--------------------------------------------------------------------------------
+  // Mogo
+  //--------------------------------------------------------------------------------
+
+  if(vexRT[Btn6U]) {
+    mogoGadget();
+  }
+  else if(vexRT[Btn6D]){
+    nogoGadget();
+  }
+
+  // if  lift is below a certain height, dont use mogo
+  // if ( currLiftHeight > LIFT_BLOCK_MOGO ) {
+    // in on 5U
+    if (vexRT[Btn5U]) {
+      motor[mogo] = 127;
+      mogoGadget();
+    }
+    //out on 5D
+    else if (vexRT[Btn5D]) {
+      motor[mogo] = -127;
+      nogoGadget();
+    }
+    else {
+      motor[mogo] = 0;
+    }
+  // }
+  // else {
+  //   motor[mogo] = 0;
+  // }
+
+  lastLiftHeight = currLiftHeight;
+  lastDeltaHeight = deltaHeight;
+
+  wait1Msec(20);
+}
